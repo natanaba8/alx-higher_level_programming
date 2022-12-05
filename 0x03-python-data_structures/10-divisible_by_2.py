b06326a015fd4dfeb9a37c24e2a06d9945e436cf
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 <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>
2022-12-05 04:36:41 +0000

    py

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -6,6 +6,6 @@ def divisible_by_2(my_list=[]):
     for i in my_list:
         if i % 2:
             list = list + [False]
-    else:
-        list = list + [True]
+        else:
+            list = list + [True]
     return list
