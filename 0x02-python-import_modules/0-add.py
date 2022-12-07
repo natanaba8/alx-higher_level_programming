15852e4f03bc8eeba5252eea350e6513fa82e0b6
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 <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>
2022-12-07 20:04:19 +0000

    taskaged

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,10 +1,16 @@
 #!/usr/bin/python3
 
+
+
 if __name__ == "__main__":
 
-        from add_0 import add
+    """Print the sum of 1 and 2."""
 
-        a = 1
-        b = 2
+    from add_0 import add
 
-        print("{} + {} = {}".format(a, b, add(a, b)))
+    a = 1
+
+    b = 2
+
+    print("{} + {} = {}".format(a, b, add(a, b)))
+
