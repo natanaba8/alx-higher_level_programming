893781284fc260d4ca83d5e6b6a8b7cf8ba4b10f
natanaba8 <natanaba8@gmail.com>
2022-12-21 17:00:45 -0800

    pp

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+
+def safe_print_division(a, b):
+    try:
+        num_div = a / b
+    except (TypeError, ZeroDivisionError):
+        num_div = None
+    finally:
+         print("Inside result: {}".format(num_div))
+    return num_div
+
