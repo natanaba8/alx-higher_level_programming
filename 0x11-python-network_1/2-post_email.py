cd6aaab6a0cf5656d276af7b991f67db30b01ed5
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 <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>
2023-03-31 18:41:27 -0700

    net

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,4 +1,4 @@
-i#!/usr/bin/python3
+#!/usr/bin/python3
 """
 Python script that takes in a URL, sends a request to the URL and displays
  the value of the X-Request-Id variable found in the header of the response
@@ -8,6 +8,6 @@ import urllib.parse
 from sys import argv
 
 if __name__ == "__main__":
-    with urllib.request.urlopen(argv[1]) as response:
-        s = response.info()
-        print(s.get('X-Request-Id'))
+    post_data = urllib.parse.urlencode({'email': argv[2]}).encode('ascii')
+    with urllib.request.urlopen(url=argv[1], data=post_data) as response:
+        print(response.read().decode('UTF-8'))
