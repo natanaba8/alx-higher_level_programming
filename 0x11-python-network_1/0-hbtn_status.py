ec8e9ad54927419664c64ed1195b9bfbafc0df72
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 <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>
2023-03-31 18:36:11 -0700

    net

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
-"""fetches https://intranet.hbtn.io/status"""
+"""Python script that fetches https://intranet.hbtn.io/status."""
+
 from urllib import request
 
 if __name__ == "__main__":
-    with request.urlopen("https://intranet.hbtn.io/status") as response:
-        response = response.read()
+    with request.urlopen("https://intranet.hbtn.io/status") as page:
+        content = page.read()
         print("Body response:")
-        print("\t- type: {}".format(type(response)))
-        print("\t- content: {}".format(response))
-        print("\t- utf8 content: {}".format(response.decode(encoding='utf-8')))
-
+        print("\t- type: {}".format(type(content)))
+        print("\t- content: {}".format(content))
+        print("\t- utf8 content: {}".format(content.decode('utf-8')))
